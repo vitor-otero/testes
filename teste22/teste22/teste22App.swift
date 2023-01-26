2429b092287fd9a8cf09b89543fd5bcb149852d7
Vitor Otero <votero.ext@gmv.com>
2023-01-26 14:41:17 +0000

    project start

---
+++ teste22/teste22/teste22App.swift
@@ -0,0 +1,17 @@
+//
+//  teste22App.swift
+//  teste22
+//
+//  Created by Vitor Otero on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct teste22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
